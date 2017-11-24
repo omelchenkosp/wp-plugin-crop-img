1137937f6d9499474450931024f15976325592b5
andrew_omelchenko <omelchenko.sp@gmail.com>
2017-11-24 17:25:21 +0200

    fix

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ Description: Simple plugin for preview and croping images before uploading.
 */
 function bye1() {
 
-	$mmm = 'Plugin activated';
-	$fp = fopen("roooo-plug.txt", "w");
-	fwrite($fp, $mmm);
-	fclose($fp);
+    $mmm = 'Plugin activated';
+    $fp = fopen("roooo-plug.txt", "w");
+    fwrite($fp, $mmm);
+    fclose($fp);
 }
 register_activation_hook(__FILE__, 'bye1');
 wp_deregister_script('jquery');
@@ -22,33 +22,75 @@ wp_enqueue_script( 'cropit', plugins_url() . '/wp-plugin-crop-img/jquery.cropit.
 add_action( 'wp_ajax_nopriv_update_business_gallery',  'update_business_gallery' );
 add_action( 'wp_ajax_update_business_gallery', 'update_business_gallery' );
 function update_business_gallery() {
-    //header("Content-type:application/json"); 
-    // $photo_id = $_POST["photo_id"];
-    $photo_id = 2;
-    // $business_id = $_POST["business_id"];
-    $business_id = 4;
+    $image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $_POST["file"]));
+    $upload_dir = wp_upload_dir();
+
+    // @new
+    $upload_path = str_replace( '/', DIRECTORY_SEPARATOR, $upload_dir['path'] ) . DIRECTORY_SEPARATOR;
+
+    $decoded = $image;
+    $filename = 'my-base64-image.png';
+
+    $hashed_filename = md5( $filename . microtime() ) . '_' . $filename;
+
+    // @new
+    $image_upload = file_put_contents( $upload_path . $hashed_filename, $decoded );
+
+    //HANDLE UPLOADED FILE
+    if( !function_exists( 'wp_handle_sideload' ) ) {
+      require_once( ABSPATH . 'wp-admin/includes/file.php' );
+    }
+
+    // Without that I'm getting a debug error!?
+    if( !function_exists( 'wp_get_current_user' ) ) {
+      require_once( ABSPATH . 'wp-includes/pluggable.php' );
+    }
     
-    //Upload la photo dans le dossier
+    // require_once ABSPATH . 'wp-admin/includes/image.php';
+    // require_once ABSPATH . 'wp-admin/includes/file.php';
+    // require_once ABSPATH . 'wp-admin/includes/media.php';
+
+    // @new
+    $file             = array();
+    $file['error']    = '';
+    $file['tmp_name'] = $upload_path . $hashed_filename;
+    $file['name']     = $hashed_filename;
+    $file['type']     = 'image/png';
+    $file['size']     = filesize( $upload_path . $hashed_filename );
+
+    // upload file to server
+    // @new use $file instead of $image_upload
+    $file_return = wp_handle_sideload( $file, array( 'test_form' => false ) );
+
+    $filename = $file_return['file'];
+    $attachment = array(
+        'post_mime_type' => $file_return['type'],
+        'post_title' => preg_replace('/\.[^.]+$/', '', basename($filename)),
+        'post_content' => '',
+        'post_status' => 'inherit',
+        'guid' => wp_upload_dir()['url'] . basename($filename)
+    );
+    $attach_id = wp_insert_attachment( $attachment, $filename );
+    $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+    wp_update_attachment_metadata( $attach_id, $attach_data );
+}
+
+function update_business_gallery3() {
+    $image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $_POST["file"]));
     require_once(ABSPATH.'wp-admin/includes/file.php');
-    $uploadedfile = $_FILES["file"];
-
-    $mmm = json_encode($uploadedfile);
-	$fp = fopen("roooo-999.txt", "w");
-	fwrite($fp, $mmm);
-	fclose($fp);
-
+    $upload_dir = wp_upload_dir();
+    $uploadedfile = file_put_contents(str_replace( '/', DIRECTORY_SEPARATOR, $upload_dir['path'] ) . DIRECTORY_SEPARATOR.'/image.png', $image);
     $movefile = wp_handle_upload($uploadedfile, array('test_form' => false));
-
     //On sauvegarde la photo dans le média library
     if ($movefile) {
         $wp_upload_dir = wp_upload_dir();
         $guid = $wp_upload_dir['url'].'/'.basename($movefile['file']);
         $attachment = array(
-	        'guid' => $guid,
-	        'post_mime_type' => $movefile['type'],
-	        'post_title' => preg_replace('/\.[^.]+$/', '', basename($movefile['file'])),
-	        'post_content' => '',
-	        'post_status' => 'inherit'
+            'guid'              => $guid,
+            'post_mime_type'    => $movefile['type'],
+            'post_title'        => preg_replace('/\.[^.]+$/', '', basename($movefile['file'])),
+            'post_content'      => '',
+            'post_status'       => 'inherit'
         );
         $attach_id = wp_insert_attachment($attachment, $movefile['file']);
         // generate the attachment metadata
